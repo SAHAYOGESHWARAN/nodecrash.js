359f1e965abfa40ada2c29ede7f786d476512039
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-27 10:53:47 +0530

    add nodecrash master

--- server2.js
+++ server2.js
@@ -1,2 +1,15 @@
 import { createServer } from 'http';
+const PORT = process.env.PORT;
+const users = [
+    {id: 1, name: 'saha'},
+    {id: 2, name: 'saha'},
+    {id: 3, name: 'saha'},
+];
 
+const server = createServer((req,res) => {
+
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
