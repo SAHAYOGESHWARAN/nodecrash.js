b2a00e49b166b4bd17597ab0a8fdda30b3fa3ab1
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-27 11:03:48 +0530

    add nodecrash master

--- server2.js
+++ server2.js
@@ -3,10 +3,19 @@ const PORT = process.env.PORT;
 const users = [
     {id: 1, name: 'saha'},
     {id: 2, name: 'saha'},
-    {id: 3, name: 'saha'},
+    {id: 3, name: 'gokul'},
 ];
 
 const server = createServer((req,res) => {
+    if(req.url === '/api/users' && req.method === 'GET'){
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify(users));
+       res.end();
+    }else {
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify({message: 'Route not found'}));
+       res.end();
+    }
 
 });
 
