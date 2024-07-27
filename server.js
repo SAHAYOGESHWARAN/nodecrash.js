98801619c98917a3f0591af8f966c8866fbe6dd9
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-27 09:36:42 +0530

    add nodecrash master

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import http from 'http';
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 const server = http.createServer((req, res) => {
     //res.setHeader('Content-Type', 'text/html');
-    //res.statusCode =404;
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello World </h1>');
+    //res.statusCode = 404;
+
+    console.log(req.url);
+    console.log(req.method);
+    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Hello World</h1>');
 });
 
 server.listen(PORT, () => {
