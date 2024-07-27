249d03c4ecf9d36f320de2e44a7ab96eb5bd651b
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-27 09:42:14 +0530

    add nodecrash maaster

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const server = http.createServer((req, res) => {
 
     console.log(req.url);
     console.log(req.method);
-    
+
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('<h1>Hello World</h1>');
 });
+
+
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
