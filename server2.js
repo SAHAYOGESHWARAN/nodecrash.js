dc43e8dac29cf22ebb5d4a0fdcb70458368cc2de
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-27 11:24:01 +0530

    add nodecrash master

--- server2.js
+++ server2.js
@@ -13,11 +13,18 @@ const server = createServer((req,res) => {
        res.end();
      } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET'){
         const id = req.url.split('/')[3];
-        console.log(id);
-        res.setHeader('Content-Type', 'application/json');
-        res.write(JSON.stringify({id: 1, name:'saha'}));
-        res.end();
-       }else { 
+        const user =users.find((user) => user.id === parseInt(id));
+        if(user){
+            res.setHeader('Content-Type', 'application/json');
+            res.write(JSON.stringify(user));
+           res.end();
+        }else{
+            res.setHeader('Content-Type', 'application/json');
+            res.statusCode = 404;
+            res.write(JSON.stringify({message: 'user not found'}));
+           res.end();
+        }
+    }else { 
         res.setHeader('Content-Type', 'application/json');
         res.statusCode = 404;
         res.write(JSON.stringify({message: 'Route not found'}));
