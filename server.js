5953b852dd768250bf3e27a4f3b83b44fb4fa37b
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-27 09:59:54 +0530

    add nodecrash master'

--- server.js
+++ server.js
@@ -2,23 +2,27 @@ import http from 'http';
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end('<h1>Homepage</h1>');
-    }else if(req.url ==='/about'){
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end('<h1>About</h1>');
-    }else {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('<h1>Not Found</h1>');
-
+    try {
+        // Check if GET request
+        if (req.method === 'GET') {
+            if (req.url === '/') {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end('<h1>Homepage</h1>');
+            } else if (req.url === '/about') {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end('<h1>About</h1>');
+            } else {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('<h1>Not Found</h1>');
+            }
+        } else {
+            throw new Error('Method not allowed');
+        }
+    } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'text/html' });
+        res.end('<h1>Server Error</h1>');
     }
-
-    
-   
 });
-
-
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
