ddad00f53535f47e859c264005e57f4580065644
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-27 08:43:29 +0530

    add nodecrash.js master

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const { generateRandomNumber, celciusToFahrenhei } = require('./utils');
+import { getPosts } from "./postcontroller.js";
 
-console.log(`Random Number: ${generateRandomNumber()}`);
 
-console.log(`Celsius to Fahrenheit: ${celciusToFahrenhei(0)}`);
+console.log(getPosts());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const { generateRandomNumber, celciusToFahrenhei } = require('./utils');
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+
+//console.log(`Celsius to Fahrenheit: ${celciusToFahrenhei(0)}`);
